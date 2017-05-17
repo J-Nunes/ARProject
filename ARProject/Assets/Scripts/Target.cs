6c1398946ffd660336324c2504d8bb1238783cfa
AGM16 <adri_67@hotmail.es>
2017-05-18 00:12:49 +0200

    Cleaning_Project_3

--- ARProject/Assets/Scripts/Target.cs
+++ ARProject/Assets/Scripts/Target.cs
@@ -48,7 +48,7 @@ public class Target : MonoBehaviour {
             hostage.animator.SetBool("Hostage_Die", true);
         }
 
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(0.3f);
 
         //Check if is an enemy or a hostage
         if (gameObject.GetComponent<Enemy>() != null)
